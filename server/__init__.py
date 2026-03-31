a73d33870042b6a0eb5e889201987975ee1e0f75
Sumitwarrior7 <sumitscience2017@gmail.com>
2026-03-31 23:01:07 +0530

    boiler plate code for openenv setup

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Job Scam Env environment server components."""
+
+from .job_scam_env_environment import JobScamEnvironment
+
+__all__ = ["JobScamEnvironment"]
